cade15db12c7656183f54b3b7cb31a195bf5d339
JieYi-777 <limjieyi@student.usm.my>
2022-01-22 18:48:17 +0800

    Give the comment of the Asset.java

--- src/main/java/Asset.java
+++ src/main/java/Asset.java
@@ -1,22 +1,22 @@
-class Asset {
-    private String path;
+class Asset {   //Create the class named Asset
+    private String path;        //Declare the variables
     private double width, height;
 
     public Asset(String path, double width, double height) {
-        this.path = path;
+        this.path = path;       //Set the path, width and the height of the asset
         this.width = width;
         this.height = height;        
     }
     
-    public double getWidth() {
+    public double getWidth() {      //Get the width of the asset
         return width;
     }
     
-    public double getHeight() {
+    public double getHeight() {     //Get the height of the asset
         return height;
     }
     
-    public String getPath() {
+    public String getPath() {       //Get the path of the asset
         return path;
     }
 }
