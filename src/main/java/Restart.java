ee8262c3153be070f0d8c33f74a985055f62fcf4
JieYi-777 <limjieyi@student.usm.my>
2022-01-22 19:02:13 +0800

    Give the comment of the Restart.java

--- src/main/java/Restart.java
+++ src/main/java/Restart.java
@@ -1,15 +1,15 @@
 import javafx.geometry.Rectangle2D;
-import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.canvas.GraphicsContext;     //Import libraries
 
 class Restart implements GameObject {
     private int WIDTH = 200;
-    private int HEIGHT = 200;
+    private int HEIGHT = 200;       //Set the path, width and height of the replay button image
     private Asset asset = new Asset("/images/replay.png", WIDTH, HEIGHT);
     private Sprite sprite;
 
     public Restart(double screenWidth, double screenHeight, GraphicsContext ctx) {
         sprite = new Sprite(asset);
-        sprite.setPosX(screenWidth / 2 - WIDTH / 2);
+        sprite.setPosX(screenWidth / 2 - WIDTH / 2);        //Set the position of the replay button
         sprite.setPosY(screenHeight - 200);
         sprite.setVel(0, 0);
         sprite.setCtx(ctx);
