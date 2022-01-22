94ee4b4f50df11e8c61550c424b4c12ecb606441
JieYi-777 <limjieyi@student.usm.my>
2022-01-22 19:07:50 +0800

    Give the comment of the Title.java

--- src/main/java/Title.java
+++ src/main/java/Title.java
@@ -1,15 +1,15 @@
-import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.canvas.GraphicsContext;     //Import library
 
 class Title implements GameObject {
     private int WIDTH = 300;
-    private int HEIGHT = 300;
+    private int HEIGHT = 300;       //Set the path, width and height of the title
     private Asset asset = new Asset("/images/title2.png", WIDTH, HEIGHT);
     private Sprite sprite;
 
     public Title(double screenWidth, double screenHeight, GraphicsContext ctx) {
         sprite = new Sprite(asset);
         sprite.setPosX(screenWidth / 2 - WIDTH / 2);
-        sprite.setPosY(30);
+        sprite.setPosY(30);                 //Set the position of the title
         sprite.setVel(0, 0);
         sprite.setCtx(ctx);
     }
