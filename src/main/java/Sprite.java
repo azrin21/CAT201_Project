96da0e2b0f242646f97c18c7bd84c3415e0c8804
JieYi-777 <limjieyi@student.usm.my>
2022-01-22 19:11:26 +0800

    Give the comment of the Sprite.java

--- src/main/java/Sprite.java
+++ src/main/java/Sprite.java
@@ -1,9 +1,9 @@
 import javafx.geometry.Rectangle2D;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.image.Image;
+import javafx.scene.image.Image;        //Import libraries
 
 class Sprite {
-    private Image image;
+    private Image image;            //Declare the variables
     private GraphicsContext ctx;
     private String path;
     private double posX,
@@ -21,7 +21,7 @@ class Sprite {
         image = new Image(path, width, height, false, false);
     }
 
-    public void changeImage(Asset asset) {
+    public void changeImage(Asset asset) {      //Change the image
         width = asset.getWidth();
         height = asset.getHeight();
         path = asset.getPath();
@@ -29,7 +29,7 @@ class Sprite {
         image = new Image(path, width, height, false, false);
     }
 
-    public void resizeImage(double width, double height) {
+    public void resizeImage(double width, double height) {      //Resize the image
         image = new Image(path, width, height, false, false);
     }
 
@@ -46,7 +46,7 @@ class Sprite {
         posY += velY;
     }
   
-    public void updateTimescaled(int timescale) {
+    public void updateTimescaled(int timescale) {       //Update the time scale
         posX += velX * timescale;
         posY += velY * timescale;
     }
@@ -55,53 +55,53 @@ class Sprite {
         ctx.drawImage(image, posX, posY);
     }
 
-    public void setSize(double width, double height) {
+    public void setSize(double width, double height) {      //Set the size
         this.width = width;
         this.height = height;
     }
 
-    public Rectangle2D getSize() {
+    public Rectangle2D getSize() {      //Get the size
         return new Rectangle2D(posX, posY, width, height);
     }
 
-    public void setWidth(double width) {
+    public void setWidth(double width) {        //Set the width
         this.width = width;
     }
 
-    public void setHeight(double height) {
+    public void setHeight(double height) {      //Set the height
         this.height = height;
     }
 
-    public double getWidth() {
+    public double getWidth() {      //Get the width
         return width;
     }
 
-    public double getHeight() {
+    public double getHeight() {     //Get the height
         return height;
     }
   
-    public void setPos(double posX, double posY) {
+    public void setPos(double posX, double posY) {      //Set the position
         this.posX = posX;
         this.posY = posY;
     }
 
-    public void setPosX(double posX) {
+    public void setPosX(double posX) {      //Set the position x
         this.posX = posX;
     }
 
-    public void setPosY(double posY) {
+    public void setPosY(double posY) {      //Set the position y
         this.posY = posY;
     }
 
-    public double[] getPos() {
+    public double[] getPos() {              //Get the position
         return new double[] { posX, posY };
     }
 
-    public double getPosX() {
+    public double getPosX() {           //Get the position x
         return posX;
     }
 
-    public double getPosY() {
+    public double getPosY() {           //Get the position y
         return posY;
     }
 
