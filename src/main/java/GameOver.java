789e202775118ae84d000b9852b14ac007bae478
JieYi-777 <limjieyi@student.usm.my>
2022-01-22 18:59:33 +0800

    Give the comment of the Gameover.java

--- src/main/java/GameOver.java
+++ src/main/java/GameOver.java
@@ -1,14 +1,14 @@
-import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.canvas.GraphicsContext;     //Import library
 
 class GameOver implements GameObject {
     private int WIDTH = 400;
-    private int HEIGHT = 400;
+    private int HEIGHT = 400;       //Set the path, width anf height of the game over image
     private Asset asset = new Asset("/images/game_over.png", WIDTH, HEIGHT);
     private Sprite sprite;
 
     public GameOver(double screenWidth, double screenHeight, GraphicsContext ctx) {
         sprite = new Sprite(asset);
-        sprite.setPosX(screenWidth / 2 - WIDTH / 2);
+        sprite.setPosX(screenWidth / 2 - WIDTH / 2);        //Set the position of the game over image
         sprite.setPosY(-120);
      //   sprite.setPosY(40);
         sprite.setVel(0, 0);
